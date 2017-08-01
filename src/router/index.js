432be7c48110cee86cb76625780bf7197fd358ed
wfnuser <wfnuser@126.com>
2017-08-01 21:01:36 +0800

    [FIX] token过期无法跳转登陆页bug（因为只检查了token有无）

--- src/router/index.js
+++ src/router/index.js
@@ -6,54 +6,54 @@ Vue.use(Router)
 
 const router = new Router({
 	routes: [{
-			path: '/',
-			name: 'Group',
-			component(resolve) {
-				require.ensure(['@/components/Group.vue'], () => {
-					resolve(require('@/components/Group.vue'));
-				});
-			},
-			// 设置 mata 字段，表示该字段需要验证
-			meta: {
-				requireAuth: true
-			}
+		path: '/',
+		name: 'Group',
+		component(resolve) {
+			require.ensure(['@/components/Group.vue'], () => {
+				resolve(require('@/components/Group.vue'));
+			});
 		},
-		{
-			path: '/login',
-			name: 'login',
-			component(resolve) {
-				require.ensure(['@/components/Login.vue'], () => {
-					resolve(require('@/components/Login.vue'));
-				});
-			}
-		},
-		{
-			path: '/register',
-			name: 'register',
-			component(resolve) {
-				require.ensure(['@/components/Register.vue'], () => {
-					resolve(require('@/components/Register.vue'));
-				});
-			}
-		},
-		// 简单设置404页面
-		{
-			path: '*',
-			component(resolve) {
-				require.ensure(['@/components/404.vue'], () => {
-					resolve(require('@/components/404.vue'));
-				});
-			},
-			hidden: true
+		// 设置 mata 字段，表示该字段需要验证
+		meta: {
+			requireAuth: true
 		}
+	},
+	{
+		path: '/login',
+		name: 'login',
+		component(resolve) {
+			require.ensure(['@/components/Login.vue'], () => {
+				resolve(require('@/components/Login.vue'));
+			});
+		}
+	},
+	{
+		path: '/register',
+		name: 'register',
+		component(resolve) {
+			require.ensure(['@/components/Register.vue'], () => {
+				resolve(require('@/components/Register.vue'));
+			});
+		}
+	},
+	// 简单设置404页面
+	{
+		path: '*',
+		component(resolve) {
+			require.ensure(['@/components/404.vue'], () => {
+				resolve(require('@/components/404.vue'));
+			});
+		},
+		hidden: true
+	}
 	]
 })
 
 // 验证 token，存在才跳转
 router.beforeEach((to, from, next) => {
 	let token = localStorage.getItem('token')
-	if(to.meta.requireAuth) {
-		if(token) {
+	if (to.meta.requireAuth) {
+		if (token) {
 			next()
 		} else {
 			next({
