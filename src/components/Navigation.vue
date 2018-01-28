ArkTicketTech/hooligroup

commit 32ad88c0e037f9b47b40ecc3ed94d6612929909a

fix navigator and style in groupDetail

src/components/Navigation.vue
@@ -1,5 +1,5 @@
 <template scope='scope'>
-    <el-menu theme="light" :default-active="defaultActiveIndex" mode="horizontal">
+    <el-menu theme="light" :default-active="defaultActiveIndex" mode="horizontal" @select="handleSelect">
         <span class="logo-sm"><img src="../assets/logo-small.gif"></span>
 
         <el-menu-item index="1">小组</el-menu-item>
@@ -45,15 +45,22 @@
                         message: '登出失败'
                     })
                 }
+            },
+            handleSelect(e) {
+                let routeMapping = {
+                    "1": "/",
+                    "2": "/event"
+                }
+                this.$router.push(routeMapping[e])
             }
         },
         computed: {
             defaultActiveIndex: function () {
                 var navMapping = {
                     "Group": "1",
-                    "Activity": "2"
+                    "Event": "2"
                 }
-                return navMapping[this.$route.name];
+                return navMapping[this.$route.name || "Group"];
             }
         }
     }

src/components/GroupDetail.vue
@@ -2,22 +2,26 @@
     <div class="container">
         <el-row :gutter="20">
             <el-col :span="16">
-                <el-card class="box-card">
-                    <div slot="header" class="clearfix">
-                        <span style="line-height: 36px;">{{groupInfo.name}}</span>
-                    </div>
-                    <div class="text item">
-                        {{groupInfo.description}}
-                    </div>
-                </el-card>
-                <el-card class="box-card">
-                    <div slot="header" class="clearfix">
-                        <span style="line-height: 36px;">活动列表</span>
-                    </div>
-                    <div v-for="event in groupInfo.events" class="text item">
-                        {{ event.name }}
-                    </div>
-                </el-card>
+                <el-row>
+                    <el-card class="box-card">
+                        <div slot="header" class="clearfix">
+                            <span style="line-height: 36px;">{{groupInfo.name}}</span>
+                        </div>
+                        <div class="text item">
+                            {{groupInfo.description}}
+                        </div>
+                    </el-card>
+                </el-row>
+                <el-row>
+                    <el-card class="box-card">
+                        <div slot="header" class="clearfix">
+                            <span style="line-height: 36px;">活动列表</span>
+                        </div>
+                        <div v-for="event in groupInfo.events" class="text item">
+                            {{ event.name }}
+                        </div>
+                    </el-card>
+                </el-row>
             </el-col>
             <el-col :span="8">
                 <el-card class="box-card">
@@ -107,8 +111,15 @@
     }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
     .container {
         padding: 24px;
+    }
+
+    .el-row {
+        margin-bottom: 20px;
+        &:last-child {
+            margin-bottom: 0;
+        }
     }
 </style>

commit 2e62732a31ec06f5414c7cd479b8ce1121b78aea

fix navigate logout problem caused by 32ad88c0e037f9b47b40ecc3ed94d6612929909a

src/components/Navigation.vue
@@ -1,13 +1,13 @@
 <template scope='scope'>
-    <el-menu theme="light" :default-active="defaultActiveIndex" mode="horizontal" @select="handleSelect">
+    <el-menu theme="light" router :default-active="$route.path" mode="horizontal">
         <span class="logo-sm"><img src="../assets/logo-small.gif"></span>
 
-        <el-menu-item index="1">小组</el-menu-item>
-        <el-menu-item index="2">活动</el-menu-item>
+        <el-menu-item index="/">小组</el-menu-item>
+        <el-menu-item index="/event">活动</el-menu-item>
 
-        <el-submenu index="3" class="right">
+        <el-submenu index="$route.path" class="right">
             <template slot="title">{{username}}</template>
-            <el-menu-item index="2-1" @click="logout()">登出</el-menu-item>
+            <el-menu-item index="$route.path" @click="logout()">登出</el-menu-item>
         </el-submenu>
     </el-menu>
 </template>
@@ -45,13 +45,6 @@
                         message: '登出失败'
                     })
                 }
-            },
-            handleSelect(e) {
-                let routeMapping = {
-                    "1": "/",
-                    "2": "/event"
-                }
-                this.$router.push(routeMapping[e])
             }
         },
         computed: {