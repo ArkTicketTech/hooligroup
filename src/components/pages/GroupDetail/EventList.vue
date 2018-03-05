b2869a4aa2d90bc7e6c16758645fcedc3dd76d21
wfnuser <wfnuser@126.com>
2018-03-05 20:05:23 +0800

    feature move eventlist to a single file component

---
+++ src/components/pages/GroupDetail/EventList.vue
@@ -0,0 +1,159 @@
+<template>
+    <div>
+        <el-card class="box-card">
+            <div slot="header" class="clearfix">
+                <span style="line-height: 36px;">活动列表</span>
+                <el-button style="float: right;" type="primary" @click="showEventModal" v-if="isAdmin">创建活动</el-button>
+            </div>
+            <div v-for="event in groupInfo.events" v-bind:key="event._id" class="text item">
+                <el-card class="box-card">
+                    <div slot="header" class="clearfix">
+                        <span style="line-height: 36px;">{{event.name}}</span>
+                        <el-button style="float: right; margin-left: 5px;" type="success" @click="goDetail(event._id)">详情</el-button>
+                        <el-button style="float: right; margin-left: 5px;" type="primary" @click="enrollEvent(event._id)">报名</el-button>
+                    </div>
+                    <div class="text item">
+                        报名截止时间：{{event.enroll_end_time | formatDate}}
+                        <br/> 活动时间：{{event.begin_time | formatDate}} 至 {{event.end_time | formatDate}}
+                        <br/> 活动地点： {{event.location}}
+                        <br/> 活动简介：{{event.Info}}
+                    </div>
+                </el-card>
+            </div>
+        </el-card>
+        <el-dialog title="创建活动" :visible.sync="eventModalVisible" width="30%">
+            <el-form ref="eventForm" :model="eventForm" label-width="80px">
+                <el-form-item label="活动名称">
+                    <el-input v-model="eventForm.name"></el-input>
+                </el-form-item>
+                <el-form-item label="活动地点">
+                    <el-input v-model="eventForm.location" placeholder="请选择活动区域"></el-input>
+                </el-form-item>
+                <el-form-item label="报名截止时间">
+                    <el-date-picker v-model="eventForm.enroll_end_time" type="datetime" placeholder="选择日期时间">
+                    </el-date-picker>
+                </el-form-item>
+                <el-form-item label="活动开始时间">
+                    <el-date-picker v-model="eventForm.begin_time" type="datetime" placeholder="选择日期时间">
+                    </el-date-picker>
+                </el-form-item>
+                <el-form-item label="活动截止时间">
+                    <el-date-picker v-model="eventForm.end_time" type="datetime" placeholder="选择日期时间">
+                    </el-date-picker>
+                </el-form-item>
+                <el-form-item label="活动简介">
+                    <el-input type="textarea" v-model="eventForm.info"></el-input>
+                </el-form-item>
+            </el-form>
+            <span slot="footer" class="dialog-footer">
+                <el-button @click="eventModalVisible = false">取 消</el-button>
+                <el-button type="primary" @click="createEvent">创建活动</el-button>
+            </span>
+        </el-dialog>
+    </div>
+</template>
+
+<script>
+/**
+ * @author: wfnuser
+ */
+import * as types from '../../../store/types'
+import api from '../../../axios'
+import {
+    Loading
+} from 'element-ui';
+export default {
+    name: 'EventList',
+    data() {
+        return {
+            userId: '',
+            eventForm: {
+                name: '',
+                location: '',
+                begin_time: '',
+                enroll_begin_time: '',
+                enroll_end_time: ''
+            },
+            eventModalVisible: false
+        }
+    },
+    props: {
+        groupInfo: {
+            type: Object
+        },
+        isAdmin: {
+            type: Boolean
+        }
+    },
+    mounted() {
+        this.userId = localStorage.getItem('userid')
+    },
+    computed: {
+        isInGroup: function () {
+            if (this.groupInfo.members) {
+                for (let i = 0; i < this.groupInfo.members.length; i++) {
+                    if (this.groupInfo.members[i]._id === this.userId) {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+    },
+    methods: {
+        enrollEvent(eventId) {
+            let request = {}
+            request.id = eventId
+            api.joinEvent(request).then((data) => {
+                this.$message({
+                    type: 'success',
+                    message: '报名成功'
+                })
+            }, (err) => {
+                this.$message({
+                    type: 'info',
+                    message: '报名失败'
+                })
+            })
+        },
+        goDetail(eventId) {
+            let url = '/event/' + eventId
+            this.$router.push({
+                path: url
+            })
+        },
+        showEventModal() {
+            this.eventModalVisible = true
+        },
+        createEvent() {
+            let that = this
+            let request = Object.assign({}, that.eventForm)
+            request.group = this.$router.currentRoute.params.id
+            api.createEvent(request).then((data) => {
+                that.eventForm = {
+                    name: '',
+                    location: '',
+                    begin_time: '',
+                    enroll_begin_time: '',
+                    enroll_end_time: ''
+                }
+                that.eventModalVisible = false
+                that.$message({
+                    type: 'success',
+                    message: '创建成功'
+                })
+            }, (err) => {
+                that.eventModalVisible = false
+                that.$message({
+                    type: 'info',
+                    message: '创建失败'
+                })
+            })
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/pages/GroupDetail.vue
+++ src/pages/GroupDetail.vue
@@ -12,28 +12,12 @@
                         </div>
                     </el-card>
                 </el-row>
+                <el-menu :default-active="currentPanel" mode="horizontal" @select="selectPanel">
+                    <el-menu-item index="events">活动</el-menu-item>
+                    <el-menu-item index="forum">论坛</el-menu-item>
+                </el-menu>
                 <el-row>
-                    <el-card class="box-card">
-                        <div slot="header" class="clearfix">
-                            <span style="line-height: 36px;">活动列表</span>
-                            <el-button style="float: right;" type="primary" @click="showEventModal" v-if="isAdmin">创建活动</el-button>
-                        </div>
-                        <div v-for="event in groupInfo.events" v-bind:key="event._id" class="text item">
-                            <el-card class="box-card">
-                                <div slot="header" class="clearfix">
-                                    <span style="line-height: 36px;">{{event.name}}</span>
-                                    <el-button style="float: right; margin-left: 5px;" type="success" @click="goDetail(event._id)">详情</el-button>
-                                    <el-button style="float: right; margin-left: 5px;" type="primary" @click="enrollEvent(event._id)">报名</el-button>
-                                </div>
-                                <div class="text item">
-                                    报名截止时间：{{event.enroll_end_time | formatDate}}
-                                    <br/> 活动时间：{{event.begin_time | formatDate}} 至 {{event.end_time | formatDate}}
-                                    <br/> 活动地点： {{event.location}}
-                                    <br/> 活动简介：{{event.Info}}
-                                </div>
-                            </el-card>
-                        </div>
-                    </el-card>
+                    <event-list :groupInfo="groupInfo" :isAdmin="isAdmin"></event-list>
                 </el-row>
             </el-col>
             <el-col :span="8">
@@ -49,35 +33,6 @@
                 </el-card>
             </el-col>
         </el-row>
-        <el-dialog title="创建活动" :visible.sync="eventModalVisible" width="30%">
-            <el-form ref="eventForm" :model="eventForm" label-width="80px">
-                <el-form-item label="活动名称">
-                    <el-input v-model="eventForm.name"></el-input>
-                </el-form-item>
-                <el-form-item label="活动地点">
-                    <el-input v-model="eventForm.location" placeholder="请选择活动区域"></el-input>
-                </el-form-item>
-                <el-form-item label="报名截止时间">
-                    <el-date-picker v-model="eventForm.enroll_end_time" type="datetime" placeholder="选择日期时间">
-                    </el-date-picker>
-                </el-form-item>
-                <el-form-item label="活动开始时间">
-                    <el-date-picker v-model="eventForm.begin_time" type="datetime" placeholder="选择日期时间">
-                    </el-date-picker>
-                </el-form-item>
-                <el-form-item label="活动截止时间">
-                    <el-date-picker v-model="eventForm.end_time" type="datetime" placeholder="选择日期时间">
-                    </el-date-picker>
-                </el-form-item>
-                <el-form-item label="活动简介">
-                    <el-input type="textarea" v-model="eventForm.info"></el-input>
-                </el-form-item>
-            </el-form>
-            <span slot="footer" class="dialog-footer">
-                <el-button @click="eventModalVisible = false">取 消</el-button>
-                <el-button type="primary" @click="createEvent">创建活动</el-button>
-            </span>
-        </el-dialog>
     </div>
 </template>
 
@@ -90,27 +45,22 @@ import api from '../axios'
 import {
     Loading
 } from 'element-ui';
+import EventList from '../components/pages/GroupDetail/EventList'
 export default {
     name: 'group',
     data() {
         return {
             msg: 'Welcome to Hooli Group',
-            username: '',
             userId: '',
             groupInfo: {},
-            eventForm: {
-                name: '',
-                location: '',
-                begin_time: '',
-                enroll_begin_time: '',
-                enroll_end_time: ''
-            },
             isAdmin: false,
-            eventModalVisible: false
+            currentPanel: 'events'
         }
     },
+    components: {
+        EventList
+    },
     mounted() {
-        this.username = localStorage.getItem('username')
         this.userId = localStorage.getItem('userid')
         this.getGroupInfo()
     },
@@ -145,21 +95,6 @@ export default {
                 loadingInstance.close()
             })
         },
-        logout() {
-            this.$store.dispatch('UserLogout')
-            if (!this.$store.state.token) {
-                this.$router.push('/login')
-                this.$message({
-                    type: 'success',
-                    message: '登出成功'
-                })
-            } else {
-                this.$message({
-                    type: 'info',
-                    message: '登出失败'
-                })
-            }
-        },
         enroll(groupId) {
             let request = {}
             request.id = groupId
@@ -175,54 +110,8 @@ export default {
                 })
             })
         },
-        enrollEvent(eventId) {
-            let request = {}
-            request.id = eventId
-            api.joinEvent(request).then((data) => {
-                this.$message({
-                    type: 'success',
-                    message: '报名成功'
-                })
-            }, (err) => {
-                this.$message({
-                    type: 'info',
-                    message: '报名失败'
-                })
-            })
-        },
-        goDetail(eventId) {
-            let url = '/event/' + eventId
-            this.$router.push({
-                path: url
-            })
-        },
-        showEventModal() {
-            this.eventModalVisible = true
-        },
-        createEvent() {
-            let that = this
-            let request = Object.assign({}, that.eventForm)
-            request.group = this.$router.currentRoute.params.id
-            api.createEvent(request).then((data) => {
-                that.eventForm = {
-                    name: '',
-                    location: '',
-                    begin_time: '',
-                    enroll_begin_time: '',
-                    enroll_end_time: ''
-                }
-                that.eventModalVisible = false
-                that.$message({
-                    type: 'success',
-                    message: '创建成功'
-                })
-            }, (err) => {
-                that.eventModalVisible = false
-                that.$message({
-                    type: 'info',
-                    message: '创建失败'
-                })
-            })
+        selectPanel(key, keyPath) {
+            this.currentPanel = key
         }
     }
 }
