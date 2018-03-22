8096fb6863fc9c342defe8a41cd2b000e0ffc720
haoliang <1259799112@qq.com>
2018-03-22 20:39:25 +0800

    add-update

--- server/users/router.js
+++ server/users/router.js
@@ -5,6 +5,8 @@ module.exports = (router) => {
 	router.post('/register', userController.Register),
 		router.post('/login', userController.Login),
 		router.get('/user', checkToken, userController.User),
+		router.post('/updateUser', userController.UpdateUserInfo),
+		router.post('/updatePassword', userController.UpdatePassword),
 		router.post('/delUser', checkToken, userController.DelUser),
 		router.post('/joinGroup', checkToken, userController.JoinGroup),
 		router.post('/confirmJoinGroup', checkToken, userController.ConfirmJoinGroup),
